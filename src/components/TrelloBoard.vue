fffaebe83a62a2ac4e8916f1c0b99ccc529c0410
Srushti-9 <srushtisuvarna77@gmail.com>
2024-03-23 23:08:11 +0530

    Implemented all the functionalities

--- src/components/TrelloBoard.vue
+++ src/components/TrelloBoard.vue
@@ -2,8 +2,13 @@
     import { nanoid } from "nanoid";
     import draggableComponent from "vuedraggable";
     import {ref} from "vue";
-    import { Column } from '~/types';
+    import { Column, Task} from '~/types';
     import TrelloBoardTask from "./TrelloBoardTask.vue";
+    import DragHandle from "./DragHandle.vue";
+    import NewTask from "./NewTask.vue";
+    import { nextTick } from 'vue'
+
+    import { useKeyModifier } from "@vueuse/core";
     const columns = ref<Column[]>([
         {
             id: nanoid(),
@@ -48,34 +53,83 @@
         },
     ]);
 
+    const alt = useKeyModifier("Alt")
+
+    function createColumn(){
+        const column: Column = {
+            id:nanoid(),
+            title: "",
+            tasks: [],
+        }
+
+        columns.value.push(column);
+
+        nextTick(() => {
+            (document.querySelector(".column:last-of-type .title-input") as HTMLInputElement).focus()
+        });
+        
+    }
+
 </script>
 
 <template>
-    <div>
+    <div class="flex items-start overflow-x-auto gap-4">
         <draggableComponent
         v-model="columns"
         group="columns"
         item-key="id"
-        class="flex gap-4 overflow-x-auto items-start">
+        :animation="150"
+        handle=".drag-handle"
+        class="flex gap-4 items-start">
 
-            <template #item="">
+            <template #item="{element: column}: {element: Column}">
+                <div 
+                class="bg-gray-200 column p-5 rounded min-w-[250px]">
+                    <header class="font-bold mb-4">
+                        <DragHandle/>
+                        <input
+                            class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+                            @keyup.enter="($event.target as HTMLInputElement).blur()"
+                            @keydown.backspace="
+                                column.title === ''
+                                ? (columns = columns.filter((c) => c.id !== column.id))
+                                : null
+                            "
+                            type="text"
+                            v-model="column.title"
+                        />
+                        <!-- {{ column.title }} -->
+                    </header>
+                    <draggableComponent
+                        v-model="column.tasks"
+                        :group="{name: 'tasks', pull: alt ? 'clone' : true}"
+                        item-key="id"
+                        :animation="150"
+                        handle=".drag-handle">
 
+                        <template #item="{element: task}: {element: Task}">
+                            <div>
+                                <TrelloBoardTask :task="task" @delete="column.tasks = column.tasks.filter((t) => t.id !== $event)"/>
+                            </div>
+                        </template>
+                   
+                    </draggableComponent>
+                    
+                    
+                    <footer>
+                        <!-- <button class="text-gray-500">+Add a card</button> -->
+                        <new-task @add="column.tasks.push($event)"/>
+                    </footer>
+                </div>
             </template>
-
         </draggableComponent>
-        <div 
-        class="bg-gray-200 column p-5 rounded min-w-[250px]"
-        v-for="column in columns" :key="column.id">
-            <header class="font-bold mb-4">
-                {{ column.title }}
-            </header>
-            <!-- Trello board task component -->
-            <TrelloBoardTask v-for="task in column.tasks" :task="task" :key="task.id"
-            >
-            </TrelloBoardTask>
-            <footer>
-                    <button class="text-gray-500">+Add a card</button>
-                </footer>
-        </div>
+        <!-- <pre>
+            {{ columns }}
+        </pre> -->
+        <button 
+        @click="createColumn"
+        class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50">
+            + Add Another Column
+        </button>
     </div>
 </template>
--- src/components/TrelloBoardTask.vue
+++ src/components/TrelloBoardTask.vue
@@ -1,16 +1,62 @@
 <script setup lang="ts">
-    import type {Task} from "@/types";
-    defineProps<{
+    import {ref} from "vue";
+    import type {Task, ID} from "@/types";
+    import DragHandle from "./DragHandle.vue";
+    import { onKeyStroke } from "@vueuse/core/index.cjs";
+    import { defineProps,defineEmits } from "vue/dist/vue.js";
+    
+    const props = defineProps<{
         task: Task,
     }>();
+    
+
+    const emit = defineEmits<{
+        (e: "delete", payload: ID ): void;
+    }>();
+
+    const focused = ref(false);
+    onKeyStroke("Backspace",(e)=>{
+        if(focused.value){
+            emit("delete",props.task.id)
+        }
+    })
 </script>
 
 <template>
     <div
     :title="task.createdAt.toLocaleString()"
-    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px]">
-    <span>
-        {{ task.title }}
-    </span>
+    class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+    @focus="focused = true"
+    @blur="focused = false"
+    tabindex="0">
+        <drag-handle class="pr-2"/>
+        <span>
+            {{ task.title }}
+        </span>
     </div>
 </template>
+
+<style>
+.sortable-chosen{
+
+}
+
+.sortable-drag .task{
+    transform: rotate(5deg);
+}
+
+.sortable-ghost .task{
+    position: relative;
+}
+
+.sortable-ghost .task::after{
+    content: "";
+    @apply absolute top-0 bottom-0 left-0 right-0 bg-slate-300 rounded;
+}
+
+.task:focus,
+.task:focus-visible{
+    @apply outline-gray-400 !important;
+    outline: gray auto 1px;
+}
+</style>
